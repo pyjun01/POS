120635b3a4885b6a272075283c832a1117f32637
박용준 <32596517+pyjun01@users.noreply.github.com>
2018-06-01 11:46:45 +0900

    :D

--- js/mmaj.js
+++ js/mmaj.js
@@ -1,13 +1,19 @@
-var dddd;
-$.ajax({
-    url: "./mm.php",
-    type: "get",
-    data: {
+let mm = document.querySelectorAll("a[href='#memo']");
+var d;
+mm[0].addEventListener("click",function(){
+    $.ajax({
+        url: "./mm.php",
+        type: "get",
+        data: {
 
-    },
-    dataType: "json",
-    success: function(data){
-        console.log(data);
-        dddd=data;
-    }
+        },
+        dataType: "json",
+        success: function(data){
+            console.log(data);
+            console.log(data.length);
+            for(i=0;i<data.length;i++){
+                document.querySelector("#memo>.modal_main").innerHTML+=`<p>${data[i][0]},${data[i][1]}</p>`;
+            }
+        }
+    });
 });
